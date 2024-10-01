0de09ea2dec3a8726bc4ade82cf155f7637e8ba8
Mangix01 <tarqui.jose.860@gmail.com>
2024-10-01 10:21:09 +0000

    Realizando validaciones en el frontend de clientes y proveedores

--- resources/views/proveedores/create.blade.php
+++ resources/views/proveedores/create.blade.php
@@ -30,82 +30,90 @@
         </div>
       </form> -->
 
-      <form action="{{ url('personas') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
-        @csrf
-        <div class="modal-body">
-            <div class="row">
-              <div class="col-lg-6 d-none">
-                <div class="form-group">
-                    <label>TIPO PERSONA(*)</label>
-                    <select name="tipo_persona" id="tipo_persona" class="form-control" required>
-                        <option {{ old('tipo_persona' ) == 'Proveedor'  ? 'selected' : '' }} value="Proveedor">Proveedor</option>
-                        
-                    </select>
-                </div>
+     <form action="{{ url('personas') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
+          @csrf
+          <div class="modal-body">
+              <div class="row">
+                  <div class="col-lg-6 d-none">
+                      <div class="form-group">
+                          <label>TIPO PERSONA(*)</label>
+                          <select name="tipo_persona" id="tipo_persona" class="form-control" required>
+                              <option {{ old('tipo_persona') == 'Proveedor' ? 'selected' : '' }} value="Proveedor">Proveedor</option>
+                          </select>
+                      </div>
+                  </div>
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="razon_social">RAZON SOCIAL *</label>
+                          <input type="text" name="razon_social" class="form-control" value="{{ old('razon_social') }}" placeholder="{{ __('Digite aquí') }} Razon Social *..." required pattern="[A-Z\s]+" title="Solo se permiten letras mayúsculas y espacios." oninput="this.value = this.value.toUpperCase();">
+                          @error('razon_social') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="tipo_documento">TIPO DOCUMENTO *</label>
+                          <select name="tipo_documento" class="form-control" required>
+                              <option value="">Seleccionar Tipo de Documento</option>
+                              <option value="CI" {{ old('tipo_documento') == 'CI' ? 'selected' : '' }}>CI</option>
+                              <option value="Licencia de conducir" {{ old('tipo_documento') == 'Licencia de conducir' ? 'selected' : '' }}>Licencia de conducir</option>
+                              <option value="NIT" {{ old('tipo_documento') == 'NIT' ? 'selected' : '' }}>NIT</option>
+                              <option value="RUC" {{ old('tipo_documento') == 'RUC' ? 'selected' : '' }}>RUC</option>
+                              <option value="Pasaporte" {{ old('tipo_documento') == 'Pasaporte' ? 'selected' : '' }}>Pasaporte</option>
+                          </select>
+                          @error('tipo_documento') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="numero_documento">NUMERO DOCUMENTO *</label>
+                          <input type="text" name="numero_documento" class="form-control" value="{{ old('numero_documento') }}" placeholder="{{ __('Digite aquí') }} Numero Documento *..." required pattern="[A-Za-z0-9]+" title="El número de documento debe ser alfanumérico.">
+                          @error('numero_documento') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="direccion">DIRECCION *</label>
+                          <input type="text" name="direccion" class="form-control" value="{{ old('direccion') }}" placeholder="{{ __('Digite aquí') }} Direccion *..." oninput="this.value = this.value.toUpperCase();" required>
+                          @error('direccion') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="telefono">TELEFONO *</label>
+                          <input type="text" name="telefono" class="form-control" value="{{ old('telefono') }}" placeholder="{{ __('Digite aquí') }} Telefono *..." required pattern="^\+?[0-9\s\-]*$" title="El teléfono solo puede contener números, +, espacios y guiones.">
+                          @error('telefono') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6"> 
+                      <div class="form-group">
+                          <label for="email">EMAIL *</label>
+                          <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="{{ __('Digite aquí') }} Email *..." required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" title="El email debe contener letras, números, puntos, guiones altos y bajos.">
+                          @error('email') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
+                      </div>
+                  </div>
+                  
+                  <div class="col-lg-6 d-none">
+                      <div class="form-group">
+                          <label>Estado(*)</label>
+                          <select name="estado" class="form-control" required>
+                              <option value="1">Activo</option>
+                              <option value="0">Inactivo</option>
+                          </select>
+                      </div>
+                  </div>
               </div>
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="razon_social">RAZON SOCIAL *</label>
-                  <input type="text" name="razon_social"  class="form-control" value="{{ old('razon_social') }}" placeholder="{{__('Digite aquí') }} Razon Social *..." required>
-                  @error('razon_social') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="tipo_documento">TIPO DOCUMENTO *</label>
-                  <input type="text" name="tipo_documento"  class="form-control" value="{{ old('tipo_documento') }}" placeholder="{{__('Digite aquí') }} Tipo Documento *..." required>
-                  @error('tipo_documento') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="numero_documento">NUMERO DOCUMENTO *</label>
-                  <input type="text" name="numero_documento"  class="form-control" value="{{ old('numero_documento') }}" placeholder="{{__('Digite aquí') }} Numero Documento *..." required>
-                  @error('numero_documento') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="direccion">DIRECCION *</label>
-                  <input type="text" name="direccion"  class="form-control" value="{{ old('direccion') }}" placeholder="{{__('Digite aquí') }} Direccion *..." required>
-                  @error('direccion') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="telefono">TELEFONO *</label>
-                  <input type="text" name="telefono"  class="form-control" value="{{ old('telefono') }}" placeholder="{{__('Digite aquí') }} Telefono *..." required>
-                  @error('telefono') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6"> 
-                <div class="form-group">
-                  <label for="email">EMAIL *</label>
-                  <input type="email" name="email"  class="form-control" value="{{ old('email') }}" placeholder="{{__('Digite aquí') }} Email *..." required>
-                  @error('email') <div style="color:#FF0000"><strong>* {{ $message }} !!</strong></div> @enderror
-                </div>
-              </div>
-              
-              <div class="col-lg-6 d-none">
-                <div class="form-group">
-                    <label>Estado(*)</label>
-                    <select name="estado" class="form-control"  required>
-                        <option value="1">Activo</option><option value="0">Inactivo</option>
-                    </select>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="modal-footer">
-          <button class="btn btn-danger" type="reset">{{__('Cancelar')}}</button>
-          <button class="btn btn-primary" type="submit">{{__('Guardar')}}</button>
-        </div>
+          </div>
+          <div class="modal-footer">
+              <button class="btn btn-danger" type="reset">{{ __('Cancelar') }}</button>
+              <button class="btn btn-primary" type="submit">{{ __('Guardar') }}</button>
+          </div>
       </form>
+
     </div>
   </div>
 </div>
